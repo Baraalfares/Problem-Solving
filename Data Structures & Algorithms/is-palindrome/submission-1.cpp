6cde31dd95a4ff3d7586ad63155fedf21b4527f7
Baraa Ali <baraalfares01@gmail.com>
2026-09-23 14:21:18 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while(left < right){
+            while(left < s.size() && !isalnum(s[left]))
+                left++;
+            while(right >= 0 && !isalnum(s[right]))
+                right--;
+            if(tolower(s[left]) != tolower(s[right]))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
